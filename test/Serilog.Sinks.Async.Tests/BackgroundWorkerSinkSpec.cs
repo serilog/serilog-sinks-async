3e5a186188ca5e0c087fb4c66fac7b940f1fc866
cocowalla <colin.anderson333@gmail.com>
2017-08-07 19:48:41 +0100

    Remove _sink field
    Instantiate sink in in test instead. Response to feedback in #21

--- test/Serilog.Sinks.Async.Tests/BackgroundWorkerSinkSpec.cs
+++ test/Serilog.Sinks.Async.Tests/BackgroundWorkerSinkSpec.cs
@@ -11,22 +11,15 @@ using Xunit;
 
 namespace Serilog.Sinks.Async.Tests
 {
-    public class BackgroundWorkerSinkSpec : IDisposable
+    public class BackgroundWorkerSinkSpec
     {
         readonly Logger _logger;
         readonly MemorySink _innerSink;
-        BackgroundWorkerSink _sink;
 
         public BackgroundWorkerSinkSpec()
         {
             _innerSink = new MemorySink();
             _logger = new LoggerConfiguration().WriteTo.Sink(_innerSink).CreateLogger();
-            _sink = new BackgroundWorkerSink(_logger, 10000, false);
-        }
-
-        public void Dispose()
-        {
-            _sink.Dispose();
         }
 
         [Fact]
@@ -38,118 +31,134 @@ namespace Serilog.Sinks.Async.Tests
         [Fact]
         public async Task WhenEmitSingle_ThenRelaysToInnerSink()
         {
-            var logEvent = CreateEvent();
-            _sink.Emit(logEvent);
+            using (var sink = this.CreateSinkWithDefaultOptions())
+            {
+                var logEvent = CreateEvent();
 
-            await Task.Delay(TimeSpan.FromSeconds(3));
+                sink.Emit(logEvent);
 
-            Assert.Equal(1, _innerSink.Events.Count);
+                await Task.Delay(TimeSpan.FromSeconds(3));
+
+                Assert.Equal(1, _innerSink.Events.Count);
+            }
         }
 
         [Fact]
         public async Task WhenInnerEmitThrows_ThenContinuesRelaysToInnerSink()
         {
-            _innerSink.ThrowAfterCollecting = true;
-
-            var events = new List<LogEvent>
+            using (var sink = this.CreateSinkWithDefaultOptions())
             {
-                CreateEvent(),
-                CreateEvent(),
-                CreateEvent()
-            };
-            events.ForEach(e => _sink.Emit(e));
+                _innerSink.ThrowAfterCollecting = true;
 
-            await Task.Delay(TimeSpan.FromSeconds(3));
+                var events = new List<LogEvent>
+                {
+                    CreateEvent(),
+                    CreateEvent(),
+                    CreateEvent()
+                };
+                events.ForEach(e => sink.Emit(e));
 
-            Assert.Equal(3, _innerSink.Events.Count);
+                await Task.Delay(TimeSpan.FromSeconds(3));
+
+                Assert.Equal(3, _innerSink.Events.Count);
+            }
         }
 
         [Fact]
         public async Task WhenEmitMultipleTimes_ThenRelaysToInnerSink()
         {
-            var events = new List<LogEvent>
+            using (var sink = this.CreateSinkWithDefaultOptions())
             {
-                CreateEvent(),
-                CreateEvent(),
-                CreateEvent()
-            };
+                var events = new List<LogEvent>
+                {
+                    CreateEvent(),
+                    CreateEvent(),
+                    CreateEvent()
+                };
+                events.ForEach(e => { sink.Emit(e); });
 
-            events.ForEach(e => { _sink.Emit(e); });
+                await Task.Delay(TimeSpan.FromSeconds(3));
 
-            await Task.Delay(TimeSpan.FromSeconds(3));
-
-            Assert.Equal(3, _innerSink.Events.Count);
+                Assert.Equal(3, _innerSink.Events.Count);
+            }
         }
 
         [Fact]
         public async Task WhenQueueFull_ThenDropsEvents()
         {
-            _sink = new BackgroundWorkerSink(_logger, 1, false);
-
-            // Cause a delay when emmitting to the inner sink, allowing us to fill the queue to capacity 
-            // after the first event is popped
-            _innerSink.DelayEmit = TimeSpan.FromMilliseconds(300);
-
-            var events = new List<LogEvent>
+            using (var sink = new BackgroundWorkerSink(_logger, 1, false))
             {
-                CreateEvent(),
-                CreateEvent(),
-                CreateEvent(),
-                CreateEvent(),
-                CreateEvent()
-            };
-            events.ForEach(e =>
-            {
-                var sw = Stopwatch.StartNew();
-                _sink.Emit(e);
-                sw.Stop();
+                // Cause a delay when emmitting to the inner sink, allowing us to fill the queue to capacity 
+                // after the first event is popped
+                _innerSink.DelayEmit = TimeSpan.FromMilliseconds(300);
 
-                Assert.True(sw.ElapsedMilliseconds < 200, "Should not block the caller when the queue is full");
-            });
+                var events = new List<LogEvent>
+                {
+                    CreateEvent(),
+                    CreateEvent(),
+                    CreateEvent(),
+                    CreateEvent(),
+                    CreateEvent()
+                };
+                events.ForEach(e =>
+                {
+                    var sw = Stopwatch.StartNew();
+                    sink.Emit(e);
+                    sw.Stop();
 
-            // If we *weren't* dropped events, the delay in the inner sink would mean the 5 events would take 
-            // at least 15 seconds to process
-            await Task.Delay(TimeSpan.FromSeconds(2));
+                    Assert.True(sw.ElapsedMilliseconds < 200, "Should not block the caller when the queue is full");
+                });
 
-            // Events should be dropped
-            Assert.Equal(2, _innerSink.Events.Count);
+                // If we *weren't* dropped events, the delay in the inner sink would mean the 5 events would take 
+                // at least 15 seconds to process
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+                // Events should be dropped
+                Assert.Equal(2, _innerSink.Events.Count);
+            }
         }
 
         [Fact]
         public async Task WhenQueueFull_ThenBlocks()
         {
-            _sink = new BackgroundWorkerSink(_logger, 1, true);
-
-            // Cause a delay when emmitting to the inner sink, allowing us to fill the queue to capacity 
-            // after the first event is popped
-            _innerSink.DelayEmit = TimeSpan.FromMilliseconds(300);
-
-            var events = new List<LogEvent>
+            using (var sink = new BackgroundWorkerSink(_logger, 1, true))
             {
-                CreateEvent(),
-                CreateEvent(),
-                CreateEvent()
-            };
+                // Cause a delay when emmitting to the inner sink, allowing us to fill the queue to capacity 
+                // after the first event is popped
+                _innerSink.DelayEmit = TimeSpan.FromMilliseconds(300);
 
-            int i = 0;
-            events.ForEach(e =>
-            {
-                var sw = Stopwatch.StartNew();
-                _sink.Emit(e);
-                sw.Stop();
-
-                // Emit should return immediately the first time, since the queue is not yet full. On 
-                // subsequent calls, the queue should be full, so we should be blocked
-                if (i > 0)
+                var events = new List<LogEvent>
                 {
-                    Assert.True(sw.ElapsedMilliseconds > 200, "Should block the caller when the queue is full");
-                }
-            });
+                    CreateEvent(),
+                    CreateEvent(),
+                    CreateEvent()
+                };
 
-            await Task.Delay(TimeSpan.FromSeconds(2));
+                int i = 0;
+                events.ForEach(e =>
+                {
+                    var sw = Stopwatch.StartNew();
+                    sink.Emit(e);
+                    sw.Stop();
 
-            // No events should be dropped
-            Assert.Equal(3, _innerSink.Events.Count);
+                    // Emit should return immediately the first time, since the queue is not yet full. On 
+                    // subsequent calls, the queue should be full, so we should be blocked
+                    if (i > 0)
+                    {
+                        Assert.True(sw.ElapsedMilliseconds > 200, "Should block the caller when the queue is full");
+                    }
+                });
+
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+                // No events should be dropped
+                Assert.Equal(3, _innerSink.Events.Count);
+            }
+        }
+
+        private BackgroundWorkerSink CreateSinkWithDefaultOptions()
+        {
+            return new BackgroundWorkerSink(_logger, 10000, false);
         }
 
         private static LogEvent CreateEvent()
