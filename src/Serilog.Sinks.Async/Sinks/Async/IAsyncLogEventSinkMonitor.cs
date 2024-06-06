4bef6f9a64275de3a3b5aa620c6f86fde7ef7b45
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2024-06-06 19:51:16 +1000

    File preamble ordering

--- src/Serilog.Sinks.Async/Sinks/Async/IAsyncLogEventSinkMonitor.cs
+++ src/Serilog.Sinks.Async/Sinks/Async/IAsyncLogEventSinkMonitor.cs
@@ -1,5 +1,4 @@
-﻿namespace Serilog.Sinks.Async;
-// Copyright © Serilog Contributors
+﻿// Copyright © Serilog Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+namespace Serilog.Sinks.Async;
 
 /// <summary>
 /// Defines a mechanism for the Async Sink to afford Health Checks a buffer metadata inspection mechanism.
